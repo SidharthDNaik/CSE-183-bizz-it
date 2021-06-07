58a4a6302dc2b2031f58a0213537b06b495d38bc
mbsiddiq <mbsiddiq@ucsc.edu>
2021-06-07 04:05:52 -0700

    Posts formatting & explore changes

---
+++ bizzit/private/secret_settings.py
@@ -0,0 +1,7 @@
+# single sign on Google (will be used if provided)
+OAUTH2GOOGLE_CLIENT_ID = "411474089416-fknot095m0ci584lrrd1ingatqtcdmeg.apps.googleusercontent.com"
+OAUTH2GOOGLE_CLIENT_SECRET = "izo5CxYClfJO8aqINpuG4ZZI"
+DB_USER="appuser",
+DB_NAME="bizz_db",
+DB_PASSWORD="O4Bpn8hhoICDy63L",
+DB_CONNECTION="bizzit:us-west2:bizzit",
