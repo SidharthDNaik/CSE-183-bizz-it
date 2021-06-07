c78d2ab1251246a9bbc046c46ca4e051af662986
Neelam Sharma <nsharm25@ucsc.edu>
2021-06-06 21:09:46 -0700

    pushing nehas pin changes

--- code/static/js/index.js
+++ code/static/js/index.js
@@ -248,6 +248,52 @@ let init = (app) => {
         Vue.set(row, 'comments_a_viewable', !row.comments_a_viewable);
     };
 
+    app.delete_comment = function(row_idx, c_idx, cs_idx){
+        let post_id = app.vue.rows[row_idx].id;
+        axios.get(delete_comment_url,
+            {params: {
+                post_id: post_id,
+                c_idx: c_idx
+            }}
+        ).then( function (response){
+            app.vue.rows[row_idx].comments.splice(cs_idx,1);
+            app.enumerate(app.vue.rows);
+        }); 
+    };
+    
+    app.add_comment = function (row_idx) {
+        // To test lets print to see if we get the comment in the field
+        // and index
+        // increase comment count for this post
+        app.vue.rows[row_idx].number_of_comments++;
+        // get id and comment
+        let id = app.vue.rows[row_idx].id;
+        let comment = app.vue.rows[row_idx].comment;
+        
+        axios.post(
+                    add_comment_url, 
+                    {
+                        post_id: id, 
+                        comment: comment, 
+                        commenter: user_name,
+                    }
+                ).then(
+                    function (response){
+                        let row = app.vue.rows[row_idx];
+                        let rows = response.data.rows;
+                        if (typeof row.comments == 'undefined'){
+                            Vue.set(row, 'comments', []);
+                        }
+                        row.comments.unshift(rows[rows.length-1]);
+                        Vue.set(row, 'email', response.data.email);
+                        app.enumerate(app.vue.rows);
+                        app.reset_comment(row_idx);
+                    });
+    }
+
+    app.reset_comment = function (row_idx) {
+        app.vue.rows[row_idx].comment = "";
+    }
 
     // We form the dictionary of all methods, so we can assign them
     // to the Vue app in a single blow.
@@ -265,6 +311,8 @@ let init = (app) => {
         select_file: app.select_file,
         upload_file: app.upload_file,
         get_category: app.get_category,
+        add_comment: app.add_comment,
+        delete_comment: app.delete_comment,
     };
 
     // This creates the Vue instance.
@@ -308,7 +356,20 @@ let init = (app) => {
                             row.string_of_dislikes = result.data.string_of_dislikes;
                         });
                     }
+                }).then(
+                    () => {
+                        for(let row of app.vue.rows) {
+                            axios.get(get_comments_stream_url, 
+                                {params: {
+                                    "post_id": row.id,
+                                }}).then(
+                                    (result) => {
+                                        row.number_of_comments += result.data.number_of_comments;
+                                        row.comments = result.data.comments;
+                                    });
+                        }
                 });
+;
 
             // app.vue.rows = rows;
     };
